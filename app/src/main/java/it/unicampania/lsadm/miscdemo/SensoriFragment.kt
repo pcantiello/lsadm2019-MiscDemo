81dfa88affce636f47a980a9f14e8007a8b20a17
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-05-11 18:38:30 +0200

    Abilitato monitoraggio temperatura ambiente

--- app/src/main/java/it/unicampania/lsadm/miscdemo/SensoriFragment.kt
+++ app/src/main/java/it/unicampania/lsadm/miscdemo/SensoriFragment.kt
@@ -3,6 +3,8 @@ package it.unicampania.lsadm.miscdemo
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -17,10 +19,13 @@ import kotlinx.android.synthetic.main.fragment_sensori.*
  * Fragment per mostrare l'utilizzo di alcuni sensori
  *
  */
-class SensoriFragment : Fragment() {
+class SensoriFragment : Fragment(), SensorEventListener {
 
     // Manager per accedere ai sensori del dispositivo
     private lateinit var sensorManager: SensorManager
+
+    // Sensori e relativi listener
+    private var tempSensor: Sensor? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +45,51 @@ class SensoriFragment : Fragment() {
         sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         // Verifico che sia presente il sensore di temperatura
-        val tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
         textTemperatura.text = if (tempSensor != null) getString(R.string.presente) else getString(R.string.non_presente)
+    }
+
+    /**
+     * Il fragment diventa attivo
+     */
+    override fun onResume() {
+        super.onResume()
+
+        // Abilito il monitoraggio del sensore di temperatura, se presente
+        tempSensor?.also { temp ->
+            sensorManager.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
+    }
+
+    /**
+     * Il fragment diventa inattivo
+     */
+    override fun onPause() {
+        super.onPause()
+
+        // Disattivo il monitoraggio del sensore di temperatura
+        sensorManager.unregisterListener(this)
+    }
+
+
+    // Implementazione interfaccia SensorEventListener
+
+    /**
+     * E' cambiata la precisione di un sensore
+     */
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Non ci interessa al momento
+    }
+
+    /**
+     * E' cambiato il valore di un sensore
+     */
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        when (event?.sensor?.type) {
+            Sensor.TYPE_AMBIENT_TEMPERATURE -> textTemperatura.text = getString(R.string.temp_format).format(event.values[0])
+        }
 
     }
 
