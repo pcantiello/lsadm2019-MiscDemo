4a9f2724052fe2908b968a86660e06e13f7bc2f9
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-05-11 19:15:15 +0200

    Creato overflow menu

--- app/src/main/java/it/unicampania/lsadm/miscdemo/MainActivity.kt
+++ app/src/main/java/it/unicampania/lsadm/miscdemo/MainActivity.kt
@@ -2,11 +2,24 @@ package it.unicampania.lsadm.miscdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Invocata quando occorre creare un menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        // Imposta il menu dal file di risorse
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+
+        return true
     }
 }
