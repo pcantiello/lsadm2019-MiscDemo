01c8cf90123a0c80e8dcc7b58b410f59c65c77b9
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-05-13 18:21:10 +0200

    Immagine acquisita

--- app/src/main/java/it/unicampania/lsadm/miscdemo/cameraFragment.kt
+++ app/src/main/java/it/unicampania/lsadm/miscdemo/cameraFragment.kt
@@ -1,11 +1,16 @@
 package it.unicampania.lsadm.miscdemo
 
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_camera.*
 
 
 /**
@@ -13,6 +18,9 @@ import android.view.ViewGroup
  * della fotocamera
  */
 class cameraFragment : Fragment() {
+
+    // Costante utilizzata per distinguere l'origine della richiesta
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +30,27 @@ class cameraFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_camera, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Imposta il funzionamento del pulsante per l'acqisizione dell'immagine
+        btnAcquisisci.setOnClickListener {
+            // Creo un intent di tipo implicito per acquisire l'immagine
+            val takePhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePhoto.resolveActivity(activity!!.packageManager)?.also {
+                startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    /**
+     * Questo metodo viene invocato per gestire il risultato al ritorno da una activity
+     * occorre determinare chi aveva generato la richiesta
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {     // Acquisizione immagine
+            val immagineCatturata = data?.extras?.get("data") as Bitmap
+            imgProfilo.setImageBitmap(immagineCatturata)
+        }
+    }
 
 }
